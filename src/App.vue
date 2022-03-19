141a2ff773da1e22f6c4a2d14d5ebb7d2d112efd
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-03-19 20:13:13 +0100

    Create login page.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Hello there</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
-    </div>
+  <div class="container mx-auto">
+    <login></login>
   </div>
-</div>
 </template>
+
+<script lang="ts" setup>
+import Login from './views/Login.vue';
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="font-sans">
+    <div class="relative min-h-screen flex flex-col sm:justify-center items-center">
+      <div class="relative sm:max-w-sm w-full">
+        <div
+          class="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"
+        ></div>
+        <div
+          class="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"
+        ></div>
+        <div class="relative w-full rounded-3xl px-6 py-4 bg-gray-100 shadow-md">
+          <form method="#" action="#" class="mt-10">
+            <div>
+              <input
+                type="text"
+                placeholder="Username"
+                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+              />
+            </div>
+
+            <div class="mt-7">
+              <input
+                type="password"
+                placeholder="Password"
+                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+              />
+            </div>
+
+            <div class="mt-7">
+              <button
+                class="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
+              >Login</button>
+            </div>
+
+            <div class="mt-7">
+              <div class="flex justify-center items-center">
+                <label class="mr-2">Are you new?</label>
+                <a
+                  href="#"
+                  class="text-blue-500 transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
+                >Create an account</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
