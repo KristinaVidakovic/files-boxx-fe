67827f07dbd7bd8576f16daea3481766aefeaf70
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-04-24 12:40:35 +0200

    Switch to createRoot API ( React 18 )

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from 'react-dom/client';
 import "tailwindcss/tailwind.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App";
 
 const queryClient = new QueryClient();
-
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
