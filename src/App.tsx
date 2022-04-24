d872991bbcaca747bbd197a22b7eacc4474d4442
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-04-24 12:35:08 +0200

    Remove react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import Login from './views/Login';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <Login />
-      </div>
-    </QueryClientProvider>
+    <div>
+      <Login />
+    </div>
   );
 }
 
