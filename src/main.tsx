c362bf548708e43d4226058e61695ffe3f6510e1
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2022-04-24 12:42:47 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,18 @@ import "tailwindcss/tailwind.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5*60*1000,
+    },
+  },
+});
+
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
